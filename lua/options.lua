92164b5929f1e44c5d04192b1632829e907336cc
Károly Török <torok.karoly.krisztian@gmail.com>
2023-05-04 14:31:58 +0200

    DAP configuration

--- lua/options.lua
+++ lua/options.lua
@@ -9,11 +9,13 @@ vim.opt.ignorecase = true;
 vim.opt.smartcase = true;
 vim.opt.smartindent = true;
 vim.opt.fileformat = 'unix';
+vim.opt.guifont = "RecMonoDuotone Nerd Font:h12"
 
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.dap.active = false
 
 vim.opt.cmdheight = 2
